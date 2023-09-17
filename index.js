589f56760e0ee15e2619a6d8df685dffc9bc2c82
divyanshu <divyanshulohar2002@gmail.com>
2023-09-17 12:41:37 +0530

    commit

--- index.js
+++ index.js
@@ -1,35 +1,51 @@
 import fs from 'fs';
+import puppeteer from 'puppeteer';
+import cheerio from 'cheerio';
+import { readFile, writeFile } from 'fs/promises';
+import csvtojson from 'csvtojson';
+import { eachOfLimit } from 'async';
 
-
-const jsonFilePath = 'output.json';
-
-fs.readFile(jsonFilePath, 'utf-8', (err, data) => {
-    if (err) {
-        console.error('Error reading JSON file:', err);
-        return;
-    }
-
+const runPuppeteer = async () => {
     try {
-        const jsonData = JSON.parse(data);
+        const browser = await puppeteer.launch({ headless: false });
+        const csvFile = await csvtojson().fromFile('core.csv');
+        const finalData = []
+        await eachOfLimit(csvFile, 10, async (row) => {
+            const page = await browser.newPage();
+            const sku = row['Core Products Item #'];
+            const url = `https://b2b.coreproducts.com/pages/search-results-page?type=article%2Cpage%2Cproduct&q=${sku}`;
+            await page.goto(url);
 
-        const dataArray = jsonData.data;
+            try {
+                await page.waitForSelector('.snize-search-results-content li', { timeout: 5000 }); // Adjust the timeout as needed
+                await page.click('.snize-search-results-content li');
 
-        dataArray.forEach((value) => {
-            // console.log('Processing value:', value);
+                await page.waitForSelector("#static-product-form-0data-product-option-0 option", { timeout: 5000 }); // Adjust the timeout as needed
 
-            const parts = value[0].split(' ');
-            const firstPart = parts[0];
+                // Use Cheerio to load the page content
+                const $ = cheerio.load(await page.content());
 
-            console.log('First part:', firstPart);
+                const optionTexts = [];
+                $("#static-product-form-0data-product-option-0 option").each((index, element) => {
+                    optionTexts.push($(element).text());
+                });
+                finalData.push({
+                    sku: sku,
+                    options: optionTexts.join(',')
+                })
 
-        });
 
-        console.log('Operations complete.');
-    } catch (parseError) {
-        console.error('Error parsing JSON:', parseError);
+            } catch (error) {
+                console.error(`Error processing SKU ${sku}:`, error);
+            }
+        })
+
+        await writeFile('output.json', JSON.stringify(finalData, null, 2));
+
+        await browser.close();
+    } catch (error) {
+        console.error("Error running Puppeteer script:", error);
     }
-});
+};
 
-
-
-
+runPuppeteer();
